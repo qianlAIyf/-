0e60786ef4c35dbe65b57704d3901b99ce25779a
qianlAIyf <37335349+qianlAIyf@users.noreply.github.com>
2019-01-09 21:08:41 +0800

    Add files via upload

--- widget.cpp
+++ widget.cpp
@@ -1,45 +1,33 @@
 #include "widget.h"
 #include "ui_widget.h"
 #include<QFileDialog>
-
-#include "videotem.h"
-#include<qmessagebox.h>
-#include<QRect>
-#include<QKeyEvent>
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    qDebug()<<QDir::currentPath();
+    this->socket = new QTcpSocket(this);
+    this->server = new QTcpServer(this);
 
-    player=new QMediaPlayer;
-    videoWidget=new videoTem;
-    ui->verticalLayout_2->addWidget(videoWidget);
-    player->setVideoOutput(videoWidget);
+        ///开启监听
+        this->server->listen(QHostAddress::Any,8765);
+        connect(this->server,SIGNAL(newConnection()),this,SLOT(acceptConnection()));
+        connect(ui->pushButton_send,SIGNAL(clicked()),this,SLOT(sendMessage()));
 
-    videoWidget->installEventFilter(this);
-    connect(ui->startButton,SIGNAL(clicked()),this,SLOT(startVideo()));
-    connect(ui->pauseButton,SIGNAL(clicked()),this,SLOT(pauseVideo()));
-    connect(ui->closeButton,SIGNAL(clicked()),this,SLOT(closeVideo()));
+        ///文件传送套接字
+    this->filesocket = new QTcpSocket(this);
+    this->fileserver = new QTcpServer(this);
 
+        this->fileserver->listen(QHostAddress::Any,8766);
+        connect(this->fileserver,SIGNAL(newConnection()),this,SLOT(acceptFileConnection()));
 
-    //创建两个套接字
-    this->tcpSocket = new QTcpSocket(this);
-    this->fileSocket = new QTcpSocket(this);
-
-    this->initTCP();
-    //显示当前路径
-    //qDebug()<<QDir::currentPath();
-        /////文件传送相关变量初始化
-        ///每次发送数据大小为64kb
-        perDataSize = 64*1024;
-        totalBytes = 0;
-        bytestoWrite = 0;
-        bytesWritten = 0;
+        //// 文件传送相关变量初始化
         bytesReceived = 0;
+        totalBytes = 0;
         filenameSize = 0;
-        //按键使能
-        this->ui->pushButton_disconnect->setEnabled(false);
+        connect(ui->pushButton_selectFile,SIGNAL(clicked()),this,SLOT(selectFile()));
+        connect(ui->pushButton_sendFile,SIGNAL(clicked()),this,SLOT(sendFile()));
 
 }
 
@@ -48,167 +36,76 @@ Widget::~Widget()
     delete ui;
 }
 
-void Widget::initTCP()
+void Widget::acceptConnection()
 {
-    //连接
-    connect(ui->pushButton_connect,SIGNAL(clicked()),this,SLOT(connectServer()));
-    qDebug()<<"test";
-    connect(ui->pushButton_send,SIGNAL(clicked()),this,SLOT(sendData()));
-
-    connect(this->ui->pushButton_openFile,SIGNAL(clicked()),this,SLOT(selectFile()));
-    connect(this->ui->pushButton_sendFile,SIGNAL(clicked()),this,SLOT(sendFile()));
+    ////返回一个socket连接
+    socket->abort();
+    this->socket = this->server->nextPendingConnection();
+    connect(socket,SIGNAL(readyRead()),this,SLOT(receiveData()));
 }
 
-void Widget::connectServer()
+void Widget::acceptFileConnection()
 {
-    tcpSocket->abort();
-    fileSocket->abort();
+    bytesWritten = 0;
+    ///每次发送数据大小为64kb
+    perDataSize = 64*1024;
+    filesocket->abort();
+    this->filesocket = this->fileserver->nextPendingConnection();
+    ///接受文件
+    connect(filesocket,SIGNAL(readyRead()),this,SLOT(updateFileProgress()));
+    //connect(filesocket,SIGNAL(bytesWritten(qint64))),this,SLOT(updateFileProgress(qint64)));
+    //connect(filesocket,SIGNAL(bytesWritten(qint64)),this,SLOT(displayError(QAbstractSocket::SocketError socketError)));
+}
 
-    tcpSocket->connectToHost("127.1.1.0",8765);
-    //连接服务器，发送文件
-    fileSocket->connectToHost("127.1.10",8766);
-
-//    tcpSocket->connectToHost("47.101.212.55",8765);
-//    //连接服务器，发送文件
-//    fileSocket->connectToHost("47.101.212.55",8766);
-
-    //自动发送一个文件到服务器
-//    this->filename = "readme.txt";
-//    sendFile();
-//    connect(fileSocket,SIGNAL(bytesWritten(qint64)),this,SLOT(updateFileProgress(qint64)));
-    /////获取当前时间
+void Widget::sendMessage()
+{
+    this->socket->write(ui->lineEdit->text().toLatin1());
+    ////显示
     current_date_time = QDateTime::currentDateTime();
-    str_date_time = current_date_time.toString("yyyy-MM-dd hh:mm:ss")+"\n";
-    QString str = "连接成功！ "+str_date_time;
-    this->ui->textEdit->append(str);
-
-
-
-    connect(tcpSocket,SIGNAL(readyRead()),this,SLOT(receiveData()));
-    connect(fileSocket,SIGNAL(readyRead()),this,SLOT(updateFileProgress()));
-
-    //更改键文字
-    this->ui->pushButton_connect->setText("已连接");
-    //按键使能
-    this->ui->pushButton_connect->setEnabled(false);
-    this->ui->pushButton_disconnect->setText("断开连接");
-    this->ui->pushButton_disconnect->setEnabled(true);
-
+    str_date_time = current_date_time.toString("yyyy-MM-dd hh:mm:ss");
+    QString str = "You "+str_date_time+"\n"+ui->lineEdit->text();
+    ui->browser->append(str);
 }
 
 void Widget::receiveData()
 {
-    //更改键文字
-    this->ui->pushButton_disconnect->setText("断开连接");
-    //按键使能
-    this->ui->pushButton_disconnect->setEnabled(true);
     /////获取当前时间
     current_date_time = QDateTime::currentDateTime();
     str_date_time = current_date_time.toString("yyyy-MM-dd hh:mm:ss")+"\n";
+
     ////接收数据
-    QString str = tcpSocket->readAll();
+    QString str = QString::fromLocal8Bit( this->socket->readAll());
+    this->filename = str;
+
     ////显示
-    str = "Server "+str_date_time+str;
-    this->ui->textEdit->append(str);
-}
+    str = "Client "+str_date_time+str;
+    this->ui->browser->append(str);
 
-void Widget::sendData()
-{
-    ////发送数据
-    QString str = ui->lineEdit->text();
-
-    this->tcpSocket->write(ui->lineEdit->text().toLocal8Bit());
-    ////显示
-    current_date_time = QDateTime::currentDateTime();
-    str_date_time = current_date_time.toString("yyyy-MM-dd hh:mm:ss");
-    str = "You "+str_date_time+"\n"+str;
-    ui->textEdit->append(str);
-}
-
-void Widget::selectFile()
-{
-
-    ////文件传送进度更新
-    connect(fileSocket,SIGNAL(bytesWritten(qint64)),this,SLOT(updateFileProgress(qint64)));
-    connect(fileSocket,SIGNAL(readyRead()),this,SLOT(updateFileProgress()));
-
-    this->ui->progressBar->setValue(0);
-    this->filename = QFileDialog::getOpenFileName(this,"Open a file","../","files (*)");
-    ui->lineEdit_filename->setText(filename);
-}
-
-void Widget::sendFile()
-{
-    this->localFile = new QFile(filename);
-    if(!localFile->open(QFile::ReadOnly))
-    {
-        ui->textEdit->append(tr("Client:open file error!"));
-        return;
-    }
-    ///获取文件大小
-    this->totalBytes = localFile->size();
-    QDataStream sendout(&outBlock,QIODevice::WriteOnly);
-    sendout.setVersion(QDataStream::Qt_4_8);
-    QString currentFileName = filename.right(filename.size()-filename.lastIndexOf('/')-1);
-
-    qDebug()<<sizeof(currentFileName);
-    ////保留总代大小信息空间、文件名大小信息空间、文件名
-    sendout<<qint64(0)<<qint64(0)<<currentFileName;
-    totalBytes += outBlock.size();
-    sendout.device()->seek(0);
-    sendout<<totalBytes<<qint64((outBlock.size()-sizeof(qint64)*2));
-
-    bytestoWrite = totalBytes-fileSocket->write(outBlock);
-    outBlock.resize(0);
-}
-
-void Widget::updateFileProgress(qint64 numBytes)
-{
-    ////已经发送的数据大小
-    bytesWritten += (int)numBytes;
-
-    ////如果已经发送了数据
-    if(bytestoWrite > 0)
-    {
-        outBlock = localFile->read(qMin(bytestoWrite,perDataSize));
-        ///发送完一次数据后还剩余数据的大小
-        bytestoWrite -= ((int)fileSocket->write(outBlock));
-        ///清空发送缓冲区
-        outBlock.resize(0);
-    }
-    else
-        localFile->close();
-
-    ////更新进度条
-    this->ui->progressBar->setMaximum(totalBytes);
-    this->ui->progressBar->setValue(bytesWritten);
-
-    ////如果发送完毕
-    if(bytesWritten == totalBytes)
-    {
-        localFile->close();
-        //fileSocket->close();
-    }
+    //传输视频
+    sendFile();
+    connect(filesocket,SIGNAL(bytesWritten(qint64)),this,SLOT(updateFileProgress(qint64)));
 }
 
 void Widget::updateFileProgress()
 {
-    QDataStream inFile(this->fileSocket);
+    QDataStream inFile(this->filesocket);
     inFile.setVersion(QDataStream::Qt_4_8);
 
     ///如果接收到的数据小于16个字节，保存到来的文件头结构
     if(bytesReceived <= sizeof(qint64)*2)
     {
-        if((fileSocket->bytesAvailable()>=(sizeof(qint64))*2) && (filenameSize==0))
+        if((filesocket->bytesAvailable()>=(sizeof(qint64))*2) && (filenameSize==0))
         {
             inFile>>totalBytes>>filenameSize;
             bytesReceived += sizeof(qint64)*2;
         }
-        if((fileSocket->bytesAvailable()>=filenameSize) && (filenameSize != 0))
+        if((filesocket->bytesAvailable()>=filenameSize) && (filenameSize != 0))
         {
             inFile>>filename;
             bytesReceived += filenameSize;
+            ////接收的文件放在指定目录下
             filename = "../"+filename;
+            qDebug()<<filename;
             localFile = new QFile(filename);
             if(!localFile->open(QFile::WriteOnly))
             {
@@ -222,64 +119,91 @@ void Widget::updateFileProgress()
     /////如果接收的数据小于总数据，则写入文件
     if(bytesReceived < totalBytes)
     {
-        bytesReceived += fileSocket->bytesAvailable();
-        inBlock = fileSocket->readAll();
+        bytesReceived += filesocket->bytesAvailable();
+        inBlock = filesocket->readAll();
         localFile->write(inBlock);
         inBlock.resize(0);
     }
-
+    ////更新进度条显示
+    this->ui->progressBar_fileProgress->setMaximum(totalBytes);
+    this->ui->progressBar_fileProgress->setValue(bytesReceived);
     ////数据接收完成时
     if(bytesReceived == totalBytes)
     {
-        this->ui->textEdit->append("Receive file successfully!");
+        this->ui->browser->append("Receive file successfully!");
         bytesReceived = 0;
         totalBytes = 0;
         filenameSize = 0;
         localFile->close();
-        //fileSocket->close();
+        //filesocket->close();
     }
 }
 
-
-
-void Widget::on_pushButton_disconnect_clicked()
+void Widget::displayError(QAbstractSocket::SocketError socketError)
 {
-    /////获取当前时间
-    current_date_time = QDateTime::currentDateTime();
-    str_date_time = current_date_time.toString("yyyy-MM-dd hh:mm:ss")+"\n";
-    QString str = "已断开连接！ "+str_date_time;
-    this->ui->textEdit->append(str);
-
-    tcpSocket->abort();
-    fileSocket->abort();
-
-    this->ui->pushButton_disconnect->setText("已断开");
-    this->ui->pushButton_disconnect->setEnabled(false);
-    this->ui->pushButton_connect->setText("连接服务器");
-    this->ui->pushButton_connect->setEnabled(true);
-    this->initTCP();
+    //qDebug()<<socket->errorString();
+    socket->close();
 }
 
-void Widget::startVideo()
+void Widget::selectFile()
 {
-    player->setMedia(QUrl::fromLocalFile("../"+ui->lineEdit->text()));
-    videoWidget->show();
-    player->play();
+    filesocket->open(QIODevice::WriteOnly);
+    ////文件传送进度更新
+    connect(filesocket,SIGNAL(bytesWritten(qint64)),this,SLOT(updateFileProgress(qint64)));
+    //qDebug()<<"test";
+    this->filename = QFileDialog::getOpenFileName(this,"Open a file","/","files (*)");
+    ui->lineEdit_fileName->setText(filename);
 }
 
-void Widget::pauseVideo()
+void Widget::sendFile()
 {
-    player->pause();
+    //qDebug()<<"test";
+    this->localFile = new QFile(filename);
+    if(!localFile->open(QFile::ReadOnly))
+    {
+        return;
+    }
+    ////文件传送进度更新
+    connect(filesocket,SIGNAL(bytesWritten(qint64)),this,SLOT(updateFileProgress(qint64)));
+
+    //qDebug()<<filename;
+    ///获取文件大小
+    this->totalBytes = localFile->size();
+    QDataStream sendout(&outBlock,QIODevice::WriteOnly);
+    sendout.setVersion(QDataStream::Qt_4_8);
+    QString currentFileName = filename.right(filename.size()-filename.lastIndexOf('/')-1);
+
+    ////保留总代大小信息空间、文件名大小信息空间、文件名
+    sendout<<qint64(0)<<qint64(0)<<currentFileName;
+    totalBytes += outBlock.size();
+    sendout.device()->seek(0);
+    sendout<<totalBytes<<qint64((outBlock.size()-sizeof(qint64)*2));
+
+    bytestoWrite = totalBytes-filesocket->write(outBlock);
+    outBlock.resize(0);
 }
 
-void Widget::closeVideo()
+void Widget::updateFileProgress(qint64 numBytes)
 {
-    player->stop();
-}
+    ////已经发送的数据大小
+    bytesWritten += (int)numBytes;
 
+    ////如果已经发送了数据
+    if(bytestoWrite > 0)
+    {
+        outBlock = localFile->read(qMin(bytestoWrite,perDataSize));
+        ///发送完一次数据后还剩余数据的大小
+        bytestoWrite -= ((int)filesocket->write(outBlock));
+        ///清空发送缓冲区
+        outBlock.resize(0);
+    }
+    else
+        localFile->close();
 
-void Widget::on_pushButton_clicked()
-{
-    videoWidget->setWindowFlags(Qt::Window);
-    videoWidget->showFullScreen();
+    ////如果发送完毕
+    if(bytesWritten == totalBytes)
+    {
+        localFile->close();
+        //filesocket->close();
+    }
 }
--- widget.h
+++ widget.h
@@ -6,15 +6,6 @@
 #include <QTcpServer>
 #include <QTcpSocket>
 
-#include<QUrl>
-#include<QSlider>
-#include<QMediaPlayer>
-#include<QVideoWidget>
-#include<QMouseEvent>
-#include<QEvent>
-#include<QRect>
-#include"videoTem.h"
-
 namespace Ui {
 class Widget;
 }
@@ -23,66 +14,50 @@ class Widget : public QWidget
 {
     Q_OBJECT
 
-public slots:
-    void startVideo();
-    void pauseVideo();
-    void closeVideo();
-
 public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
-
-    void initTCP();
-    void newConnect();
-
+    QTcpServer *server;
+        QTcpSocket *socket;
+        QTcpServer *fileserver;
+        QTcpSocket *filesocket;
 private slots:
-    ////连接服务器
-    void connectServer();
-    ////与服务器断开连接
-    void on_pushButton_disconnect_clicked();
-    ////接收服务器发送的数据
+    void sendMessage();
+    void acceptConnection();
+    ////接收客户端发送的数据
     void receiveData();
-    ////向服务器发送数据
-    void sendData();
 
-    ////浏览文件
-    void selectFile();
-    ////发送文件
-    void sendFile();
-    ////更新文件发送进度
-    void updateFileProgress(qint64);
-    ////更新文件接收进度
+    void acceptFileConnection();
     void updateFileProgress();
+    void displayError(QAbstractSocket::SocketError socketError);
 
-    void on_pushButton_clicked();
+    ///选择发送的文件
+    void selectFile();
+    void sendFile();
+    ////更新文件传送进度
+    void updateFileProgress(qint64);
+
 
 
 private:
     Ui::Widget *ui;
-    QTcpSocket *tcpSocket;
-        QTcpSocket *fileSocket;
-
-        ///文件传送
-        QFile *localFile;
-        ///文件大小
-        qint64 totalBytes;      //文件总字节数
-        qint64 bytesWritten;    //已发送的字节数
-        qint64 bytestoWrite;    //尚未发送的字节数
-        qint64 filenameSize;    //文件名字的字节数
-        qint64 bytesReceived;   //接收的字节数
+    ////传送文件相关变量
+        qint64 totalBytes;
+        qint64 bytesReceived;
+        qint64 bytestoWrite;
+        qint64 bytesWritten;
+        qint64 filenameSize;
+        QString filename;
         ///每次发送数据大小
         qint64 perDataSize;
-        QString filename;
-        ///数据缓冲区
+        QFile *localFile;
+        ////本地缓冲区
         QByteArray inBlock;
         QByteArray outBlock;
 
         ////系统时间
         QDateTime current_date_time;
         QString str_date_time;
-
-        QMediaPlayer *player;
-        videoTem *videoWidget;
 
 };
 
